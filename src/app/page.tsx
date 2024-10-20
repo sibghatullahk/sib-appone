88e39e71182c713b345eabf45a2694794062abaa
Sibghat ullah <sibghatullahk83@gmail.com>
2024-10-20 22:16:37 +0500

    Cards added 20-10-24

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,13 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <div className="flex w-full h-50">
-        {/* Cyan background container */}
-        <div className="flex w-1/2 h-50 bg-cyan-500">
+      
+      <div className="flex justify-center w-full h-auto p-2">
+        {/* Background container */}
+        <div className="flex justify-between w-11/12 p-4 h-[500px]">
+
           {/* Image Card 1 */}
-          <div className="w-1/2 p-4">
+          <div className="w-5/12 bg-indigo-600 h-400 rounded-md shadow-md p-2">
             <img
               src="/path/to/your/image1.jpg"
               alt="Image 1"
@@ -26,7 +28,7 @@ export default function Home() {
           </div>
 
           {/* Image Card 2 */}
-          <div className="w-1/2 p-4">
+          <div className="w-5/12 bg-fuchsia-500 h-400 rounded-md shadow-md p-2">
             <img
               src="/path/to/your/image2.jpg"
               alt="Image 2"
@@ -36,6 +38,44 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* the next 3 components */}
+
+      <div className="flex justify-center w-full h-auto p-2">
+        {/* Cyan background container */}
+        <div className="flex justify-between w-11/12 p-4 h-[500px]">
+          {/* Image Card 1 */}
+          <div className="w-1/4 bg-indigo-600 h-400 rounded-md shadow-md p-2">
+            <img
+              src="/path/to/your/image1.jpg"
+              alt="Image 1"
+              className="w-full h-auto object-cover"
+            />
+            <p className="text-white mt-2">Image Card 1 Description</p>
+          </div>
+
+          {/* Image Card 2 */}
+          <div className="w-1/4 bg-fuchsia-500 h-400 rounded-md shadow-md p-2">
+            <img
+              src="/path/to/your/image2.jpg"
+              alt="Image 2"
+              className="w-full h-auto object-cover"
+            />
+            <p className="text-white mt-2">Image Card 2 Description</p>
+          </div>
+
+          {/* Image Card 3 */}
+          <div className="w-1/4 bg-indigo-600 h-400 rounded-md shadow-md p-2">
+            <img
+              src="/path/to/your/image1.jpg"
+              alt="Image 1"
+              className="w-full h-auto object-cover"
+            />
+            <p className="text-white mt-2">Image Card 1 Description</p>
+          </div>
+        </div>
+      </div>
+
       <Newfoot/>
     </div>
   );
