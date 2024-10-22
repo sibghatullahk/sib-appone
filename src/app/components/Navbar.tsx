24a48aef101dd73037b8e10f03545aa30715ca53
Sibghat ullah <sibghatullahk83@gmail.com>
2024-10-22 11:09:36 +0500

    redeploy error corr

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from 'next/image';
 
 export default Navbar
 function Navbar(){
@@ -7,7 +8,7 @@ function Navbar(){
             <header>
         <nav className=" flex w-full bg-slate-500 h-20">
           <div className="flex">
-            <div className="bg-green-600 w-20 h-20"><p className=" text-pretty text-slate-50 rounded-sm"><img src="https://img.icons8.com/?size=100&id=23831&format=png&color=000000" alt="Logo"/></p>
+            <div className="bg-green-600 w-20 h-20"><p className=" text-pretty text-slate-50 rounded-sm"><Image src="https://img.icons8.com/?size=100&id=23831&format=png&color=000000" width={100} height={50} alt="Logo"/></p>
           </div>
         </div>
         <div className="flex w-3/4 justify-end items-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 import Newfoot from "./components/Newfoot";
-import About from "./about/page";
-import Contact from "./contact/page";
-import Gallery from "./gallery/page";
-import menu from "./menu/page";
-import Link from "next/link";
+// import Image from "next/image";
+// import About from "./about/page";
+// import Contact from "./contact/page";
+// import Gallery from "./gallery/page";
+// import menu from "./menu/page";
+// import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,21 +18,25 @@ export default function Home() {
 
           {/* Image Card 1 */}
           <div className="w-5/12 bg-indigo-600 h-300 rounded-md shadow-md p-2">
-            <img
+            {/* <Image
               src="/path/to/your/image1.jpg"
               alt="Image 1"
               className="w-full h-auto object-cover"
-            />
+              width={50}
+              height={50}
+            /> */}
             <p className="text-white mt-2">Image Card 1 Description</p>
           </div>
 
           {/* Image Card 2 */}
           <div className="w-5/12 bg-fuchsia-500 h-300 rounded-md shadow-md p-2">
-            <img
+            {/* <Image
               src="/path/to/your/image2.jpg"
               alt="Image 2"
               className="w-full h-auto object-cover"
-            />
+              width={50}
+              height={50}
+            /> */}
             <p className="text-white mt-2">Image Card 2 Description</p>
           </div>
         </div>
@@ -44,31 +49,37 @@ export default function Home() {
         <div className="flex justify-between w-11/12 p-4 h-[500px]">
           {/* Image Card 1 */}
           <div className="w-1/4 bg-indigo-600 h-300 rounded-md shadow-md p-2">
-            <img
+            {/* <Image
               src="/path/to/your/image1.jpg"
               alt="Image 1"
               className="w-full h-auto object-cover"
-            />
+              width={50}
+              height={50}
+            /> */}
             <p className="text-white mt-2">Image Card 1 Description</p>
           </div>
 
           {/* Image Card 2 */}
           <div className="w-1/4 bg-fuchsia-500 h-400 rounded-md shadow-md p-2">
-            <img
+            {/* <Image
               src="/path/to/your/image2.jpg"
               alt="Image 2"
               className="w-full h-auto object-cover"
-            />
+              width={50}
+              height={50}
+            /> */}
             <p className="text-white mt-2">Image Card 2 Description</p>
           </div>
 
           {/* Image Card 3 */}
           <div className="w-1/4 bg-indigo-600 h-400 rounded-md shadow-md p-2">
-            <img
+            {/* <Image
               src="/path/to/your/image1.jpg"
               alt="Image 3"
               className="w-full h-auto object-cover"
-            />
+              width={50}
+              height={50}
+            /> */}
             <p className="text-white mt-2">Image Card 3 Description</p>
           </div>
         </div>
